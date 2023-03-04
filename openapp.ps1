88d60ba8fe44c1e77f9568261678b70c0457c5a9
celebrater <tkgm45nbchk@gmail.com>
2023-03-04 19:16:10 +0900

    first commit

---
+++ openapp.ps1
@@ -0,0 +1,4 @@
+﻿
+Start-Process -filepath "C:\Program Files (x86)\sakura\sakura.exe" ".\test.txt" # SUCCESS
+#Start-Process -filepath "C:\Program Files\CELSYS\CLIP STUDIO 1.5\CLIP STUDIO PAINT\CLIPStudioPaint.exe" ".\test.png" # FAILURE
+#Start-Process -filepath "C:\Program Files\Mozilla Firefox\firefox.exe" ".\test.png" # SUCCESS
